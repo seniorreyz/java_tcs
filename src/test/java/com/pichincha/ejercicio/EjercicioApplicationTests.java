c6988ec914db4d26d80d4051b3a3e46553ea1a65
Pulul29 <psaluisa@outlook.com>
2022-04-01 10:50:26 -0500

    Codigo inicial prueba practica

---
+++ src/test/java/com/pichincha/ejercicio/EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
